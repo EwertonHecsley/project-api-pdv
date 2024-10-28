2ff053ba311b83dc4aa39a8e8a7c8dd63c6f5b4a
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-10-27 21:55:09 -0300

    feat: PrismaModule add UserRepository

--- src/infra/database/prisma/prisma.module.ts
+++ src/infra/database/prisma/prisma.module.ts
@@ -2,12 +2,15 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
 import { CategoryRepository } from "src/domain/category/repository/category.repository";
 import { CategoryPrismaRepository } from "../repositories/category.prisma.repository";
+import { UserRepository } from "src/domain/user/repository/user.repository";
+import { UserPrismaRepository } from "../repositories/user.prisma.repository";
 
 @Module({
     providers: [
         PrismaService,
-        { provide: CategoryRepository, useClass: CategoryPrismaRepository }
+        { provide: CategoryRepository, useClass: CategoryPrismaRepository },
+        { provide: UserRepository, useClass: UserPrismaRepository }
     ],
-    exports: [PrismaService, CategoryRepository]
+    exports: [PrismaService, CategoryRepository, UserRepository]
 })
 export class PrismaModule { }
